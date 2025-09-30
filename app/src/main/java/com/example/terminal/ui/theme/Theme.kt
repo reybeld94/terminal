4ea2ffcdf973db186f8427e14613a0d85ddb4539
reybeld94 <reybeld94@icloud.com>
2025-09-30 14:00:59 -0400

    Apply dark terminal color scheme

--- app/src/main/java/com/example/terminal/ui/theme/Theme.kt
+++ app/src/main/java/com/example/terminal/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.terminal.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,44 +8,39 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    background = Color(0xFF55575A),
-    surface = Color(0xFF55575A),
-    onBackground = Color(0xFFFFFFFF),
-    onSurface = Color(0xFFFFFFFF)
+    primary = TerminalPrimary,
+    onPrimary = TerminalOnColor,
+    secondary = TerminalSecondary,
+    onSecondary = TerminalOnColor,
+    tertiary = TerminalTertiary,
+    onTertiary = TerminalOnColor,
+    background = TerminalBackground,
+    onBackground = TerminalOnColor,
+    surface = TerminalSurface,
+    onSurface = TerminalOnColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Color(0xFF55575A),
-    surface = Color(0xFF55575A),
-    onBackground = Color(0xFFFFFFFF),
-    onSurface = Color(0xFFFFFFFF)
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = TerminalPrimary,
+    onPrimary = TerminalOnColor,
+    secondary = TerminalSecondary,
+    onSecondary = TerminalOnColor,
+    tertiary = TerminalTertiary,
+    onTertiary = TerminalOnColor,
+    background = TerminalBackground,
+    onBackground = TerminalOnColor,
+    surface = TerminalSurface,
+    onSurface = TerminalOnColor
 )
 
 @Composable
 fun TerminalTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
